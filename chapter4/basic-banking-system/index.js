ab2603cc88b477c3dbc9bbe0dd449413750d00a0
whywidodo <mr.wahyuwidodo@gmail.com>
2023-10-16 19:24:15 +0700

    add api documentation swagger

--- chapter4/basic-banking-system/index.js
+++ chapter4/basic-banking-system/index.js
@@ -4,10 +4,13 @@ const app = express();
 require('dotenv').config()
 const port = process.env.port || 3000;
 const router = require('./routers');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJson = require('./openapi.json');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.json({ strict: false }));
+app.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerJson));
 app.use('/api/v1', router);
 
 app.listen(port, () => {
